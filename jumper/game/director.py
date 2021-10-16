864245439fd3ecca20b9bab792657eb716bd80d1
Santiago Pintus <santiagopintus@gmail.com>
2021-10-16 17:53:53 -0300

    Created Director, WordChooser, Guesser and Console classes

---
+++ jumper/game/director.py
@@ -0,0 +1,25 @@
+# from game.guesser import Guesser
+from game.guesser import Guesser
+from game.word_chooser import WordChooser
+from game.console import Console
+
+class Director:
+    """
+    The Director class will comunicate the other classes with eachother
+    """
+
+    def __init__(self):
+
+        # self.guesser = Guesser
+        self.word = ''
+        self.keep_playing = True
+        self.chooser = WordChooser()
+        self.guesser = Guesser()
+    
+    def start_game(self):
+
+        self.word = self.chooser.choose_word()
+        
+        while self.keep_playing:
+
+
---
+++ jumper/game/guesser.py
@@ -0,0 +1,6 @@
+class Guesser:
+
+    def ask_letter():
+
+        letter = input('Guess a letter [a-z]: ')
+        return letter
